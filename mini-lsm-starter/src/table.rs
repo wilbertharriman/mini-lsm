1804be33b070e15d5b7a607dbbb4117fe63bd373
Wilbert Harriman <wilbert26harriman@gmail.com>
2023-03-26 22:05:44 +0800

    Finished day 2

--- mini-lsm-starter/src/table.rs
+++ mini-lsm-starter/src/table.rs
@@ -1,15 +1,13 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 mod builder;
 mod iterator;
 
+use std::fs::File;
 use std::path::Path;
 use std::sync::Arc;
 
 use anyhow::Result;
 pub use builder::SsTableBuilder;
-use bytes::{Buf, Bytes};
+use bytes::{Buf, BufMut, Bytes};
 pub use iterator::SsTableIterator;
 
 use crate::block::Block;
@@ -25,43 +23,65 @@ pub struct BlockMeta {
 
 impl BlockMeta {
     /// Encode block meta to a buffer.
-    pub fn encode_block_meta(
-        block_meta: &[BlockMeta],
-        #[allow(clippy::ptr_arg)] // remove this allow after you finish
-        buf: &mut Vec<u8>,
-    ) {
-        unimplemented!()
+    pub fn encode_block_meta(block_meta: &[BlockMeta], buf: &mut Vec<u8>) {
+        for meta in block_meta {
+            buf.put_u32(meta.offset as u32);
+            buf.put_u16(meta.first_key.len() as u16);
+            buf.put_slice(&meta.first_key);
+        }
     }
 
     /// Decode block meta from a buffer.
-    pub fn decode_block_meta(buf: impl Buf) -> Vec<BlockMeta> {
-        unimplemented!()
+    pub fn decode_block_meta(mut buf: impl Buf) -> Vec<BlockMeta> {
+        let mut block_meta: Vec<BlockMeta> = Vec::new();
+        while buf.has_remaining() {
+            let offset: usize = buf.get_u32() as usize;
+            let key_len: usize = buf.get_u16() as usize;
+            let key = buf.copy_to_bytes(key_len);
+            block_meta.push(BlockMeta {
+                offset,
+                first_key: key,
+            })
+        }
+        block_meta
     }
 }
 
 /// A file object.
-pub struct FileObject(Bytes);
+pub struct FileObject(File, u64);
 
 impl FileObject {
     pub fn read(&self, offset: u64, len: u64) -> Result<Vec<u8>> {
-        Ok(self.0[offset as usize..(offset + len) as usize].to_vec())
+        use std::os::unix::fs::FileExt;
+        let mut buf: Vec<u8> = vec![0; len as usize];
+        self.0.read_exact_at(&mut buf[..], offset)?;
+        Ok(buf)
     }
 
     pub fn size(&self) -> u64 {
-        self.0.len() as u64
+        self.1
     }
 
     /// Create a new file object (day 2) and write the file to the disk (day 4).
     pub fn create(path: &Path, data: Vec<u8>) -> Result<Self> {
-        unimplemented!()
+        // let mut file = File::create(path)?;
+        // file.write_all(&data)?;
+        // Ok(FileObject(file, data.len() as u64))
+        std::fs::write(path, &data)?;
+        Ok(FileObject(
+            File::options().read(true).write(false).open(path)?,
+            data.len() as u64,
+        ))
     }
 
     pub fn open(path: &Path) -> Result<Self> {
+        // File::open(path);
         unimplemented!()
     }
 }
 
 pub struct SsTable {
+    id: usize,
     file: FileObject,
     block_metas: Vec<BlockMeta>,
     block_meta_offset: usize,
@@ -75,12 +95,31 @@ impl SsTable {
 
     /// Open SSTable from a file.
     pub fn open(id: usize, block_cache: Option<Arc<BlockCache>>, file: FileObject) -> Result<Self> {
-        unimplemented!()
+        let data_len = file.1;
+        let block_meta_offset_raw = file.read(data_len - 4, 4)?;
+        let block_meta_offset: u64 = (&block_meta_offset_raw[..]).get_u32() as u64;
+
+        let block_meta_raw = file.read(block_meta_offset, data_len - 4 - block_meta_offset)?;
+
+        Ok(SsTable {
+            id,
+            file,
+            block_metas: BlockMeta::decode_block_meta(&block_meta_raw[..]),
+            block_meta_offset: block_meta_offset as usize,
+        })
     }
 
     /// Read a block from the disk.
     pub fn read_block(&self, block_idx: usize) -> Result<Arc<Block>> {
-        unimplemented!()
+        let offset = self.block_metas[block_idx].offset;
+        let next_offset = self
+            .block_metas
+            .get(block_idx + 1)
+            .map_or(self.block_meta_offset, |x| x.offset);
+        let data = self
+            .file
+            .read(offset as u64, (next_offset - offset) as u64)?;
+        Ok(Arc::new(Block::decode(&data[..])))
     }
 
     /// Read a block from disk, with block cache. (Day 4)
@@ -90,12 +129,15 @@ impl SsTable {
 
     /// Find the block that may contain `key`.
     pub fn find_block_idx(&self, key: &[u8]) -> usize {
-        unimplemented!()
+        // binary search on blockmetas
+        self.block_metas
+            .partition_point(|meta| key >= meta.first_key)
+            .saturating_sub(1)
     }
 
     /// Get number of data blocks.
     pub fn num_of_blocks(&self) -> usize {
-        unimplemented!()
+        self.block_metas.len()
     }
 }
 
--- mini-lsm-starter/src/table/builder.rs
+++ mini-lsm-starter/src/table/builder.rs
@@ -5,41 +5,81 @@ use std::path::Path;
 use std::sync::Arc;
 
 use anyhow::Result;
+use bytes::BufMut;
 
-use super::{BlockMeta, SsTable};
-use crate::lsm_storage::BlockCache;
+use super::{BlockMeta, FileObject, SsTable};
+use crate::{block::BlockBuilder, lsm_storage::BlockCache};
 
 /// Builds an SSTable from key-value pairs.
 pub struct SsTableBuilder {
     pub(super) meta: Vec<BlockMeta>,
     // Add other fields you need.
+    builder: BlockBuilder,
+    first_key: Vec<u8>,
+    data: Vec<u8>,
+    block_size: usize,
 }
 
 impl SsTableBuilder {
     /// Create a builder based on target block size.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            meta: Vec::new(),
+            builder: BlockBuilder::new(block_size),
+            first_key: Vec::new(),
+            data: Vec::new(),
+            block_size,
+        }
+    }
+
+    fn finalize_block(&mut self) {
+        let builder = std::mem::replace(&mut self.builder, BlockBuilder::new(self.block_size));
+        let encoded_block = builder.build().encode();
+        self.meta.push(BlockMeta {
+            offset: self.data.len(),
+            first_key: std::mem::take(&mut self.first_key).into(),
+        });
+        self.data.extend(encoded_block);
     }
 
     /// Adds a key-value pair to SSTable
     pub fn add(&mut self, key: &[u8], value: &[u8]) {
-        unimplemented!()
+        if self.first_key.is_empty() {
+            self.first_key = key.to_vec();
+        }
+
+        if !self.builder.add(key, value) {
+            self.finalize_block();
+            self.first_key = key.to_vec();
+            assert!(self.builder.add(key, value));
+        }
     }
 
     /// Get the estimated size of the SSTable.
     pub fn estimated_size(&self) -> usize {
-        unimplemented!()
+        self.data.len()
     }
 
     /// Builds the SSTable and writes it to the given path. No need to actually write to disk until
     /// chapter 4 block cache.
     pub fn build(
-        self,
+        mut self,
         id: usize,
         block_cache: Option<Arc<BlockCache>>,
         path: impl AsRef<Path>,
     ) -> Result<SsTable> {
-        unimplemented!()
+        self.finalize_block();
+        let mut buf = self.data;
+        let meta_offset = buf.len();
+        BlockMeta::encode_block_meta(&self.meta, &mut buf);
+        buf.put_u32(meta_offset as u32);
+        let file = FileObject::create(path.as_ref(), buf)?;
+        Ok(SsTable {
+            id,
+            file,
+            block_metas: self.meta,
+            block_meta_offset: meta_offset,
+        })
     }
 
     #[cfg(test)]
